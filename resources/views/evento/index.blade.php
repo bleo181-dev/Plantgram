9280ec18656fe2193e0c216646710f4985aa4ad2
riccardo <riccardo01.luppi@edu.unife.it>
2022-01-03 00:30:42 +0100

    progressbar funzionante

--- resources/views/evento/index.blade.php
+++ resources/views/evento/index.blade.php
@@ -1,25 +1,26 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
+<div class="container col-md-8">
     <div class="row justify-content-center">
-        <div class="card">
-            <div class="card-header">{{ __('Eventi') }}</div>
-
-                <div class="card-body">
-                    @foreach($eventi as $evento)
-                        <div class="row">
-                            <div class="progress w-75">
-                                <div class="progress-bar" role="progressbar" style="width: {{ ((strtotime(date('Y-m-d H:i:s'))-strtotime($evento->updated_at))*100)/$evento->cadenza }}%" aria-valuenow="{{ strtotime(date('Y-m-d H:i:s'))-strtotime($evento->updated_at) }}" aria-valuemin="0" aria-valuemax="{{$evento->cadenza}}">{{$evento->nome}}</div>
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Eventi') }}</div>
+                    <div class="card-body">
+                        @foreach($eventi as $evento)
+                            <div class="row">
+                                <div class="progress w-75">
+                                    <div class="progress-bar" role="progressbar" style="width: {{((strtotime('now')-strtotime($evento->data))*100)/$evento->cadenza }}%" aria-valuenow="{{strtotime(date('Y-m-d H:i:s'))-strtotime($evento->data) }}" aria-valuemin="0" aria-valuemax="{{$evento->cadenza}}">{{$evento->nome}}</div>
+                                </div>
+                                <form action="{{ URL::action('EventoController@update', $evento->codice_evento) }}" method="POST" class="col-md-2">
+                                    {{ csrf_field() }}
+                                    @method('PUT')
+                                    <input type="hidden" name="nome" value="{{$evento->nome}}">
+                                    <button class="btn btn-primary" type="submit">{{$evento->nome}}</button>
+                                </form>
                             </div>
-                            <form action="{{ URL::action('EventoController@update', $evento->codice_evento) }}" method="POST" class="col-md-2">
-                                {{ csrf_field() }}
-                                @method('PUT')
-                                <input type="hidden" name="nome" value="{{$evento->nome}}">
-                                <button class="btn btn-primary" type="submit">{{$evento->nome}}</button>
-                            </form>
-                        </div>
-                    @endforeach
+                        @endforeach
+                    </div>
                 </div>
             </div>
         </div>
