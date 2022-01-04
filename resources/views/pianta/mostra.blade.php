a20411008e5332cd7dd0e57e20ec18e81812e0a0
riccardo <riccardo01.luppi@edu.unife.it>
2022-01-04 19:44:22 +0100

    pianta show

--- resources/views/pianta/mostra.blade.php
+++ resources/views/pianta/mostra.blade.php
@@ -18,26 +18,26 @@
                                 <h5 class="card-title">{{$pianta->nome}}</h5>
                                 <p class="card-text">{{$pianta->luogo}}</p>
                                 <p class="card-text"><small class="text-muted"></small></p>
-                                @foreach($eventi as $evento)
-                                    <div class="row justify-content-center">
-                                        <div class="progress w-75">
-                                            <div class="progress-bar" role="progressbar" style="width: {{((strtotime('now')-strtotime($evento->data))*100)/$evento->cadenza }}%" aria-valuenow="{{strtotime(date('Y-m-d H:i:s'))-strtotime($evento->data) }}" aria-valuemin="0" aria-valuemax="{{$evento->cadenza}}">{{$evento->nome}}</div>
-                                        </div>
-                                        <form action="{{ URL::action('EventoController@update', $evento->codice_evento) }}" method="POST" class="col-md-2">
-                                            {{ csrf_field() }}
-                                            @method('PUT')
-                                            <input type="hidden" name="nome" value="{{$evento->nome}}">
-                                            <button class="btn btn-primary" type="submit">{{$evento->nome}}</button>
-                                        </form>
-                                    </div>
-                                    <br>
-                                @endforeach
                             </div>
                         </div>
                     </div>
-                    <div class="row justify-content-center">
-                        <h5>Diario</h5>
-                    </div>
+                    <br>
+                    <hr>
+                    <br>
+                    @foreach($eventi as $evento)
+                        <div class="row justify-content-center">
+                            <div class="progress w-75">
+                                <div class="progress-bar" role="progressbar" style="width: {{((strtotime('now')-strtotime($evento->data))*100)/$evento->cadenza }}%" aria-valuenow="{{strtotime(date('Y-m-d H:i:s'))-strtotime($evento->data) }}" aria-valuemin="0" aria-valuemax="{{$evento->cadenza}}">{{$evento->nome}}</div>
+                            </div>
+                            <form action="{{ URL::action('EventoController@update', $evento->codice_evento) }}" method="POST" class="col-md-2">
+                                {{ csrf_field() }}
+                                @method('PUT')
+                                <input type="hidden" name="nome" value="{{$evento->nome}}">
+                                <button class="btn btn-primary" type="submit">{{$evento->nome}}</button>
+                            </form>
+                        </div>
+                        <br>
+                    @endforeach
                     @foreach($diario as $i)
                         @include('diario.diariopost')
                     @endforeach
