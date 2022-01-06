0d01f93c08ebb4df5053c7dae69f367c65332ffb
Lorenzo Antrenucci <lorenzo.antenucci@edu.unife.it>
2022-01-06 16:27:17 +0100

    Aggiunti controlli per la sicurezza e possibilità di riprendere creazione della serra anche dopo esserti sloggato

--- resources/views/layouts/app_lending.blade.php
+++ resources/views/layouts/app_lending.blade.php
@@ -89,6 +89,7 @@
                                                      document.getElementById('logout-form').submit();">
                                         {{ __('Logout') }}
                                     </a>
+                                    <a class="dropdown-item" href="{{ URL::action('UserController@edit', Auth()->id()) }}">Modifica profilo</a>
 
                                     <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                                         @csrf
