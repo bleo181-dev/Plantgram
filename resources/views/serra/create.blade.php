86d5c874076efd53fb36361f763b34a9b650edeb
Lorenzo Antrenucci <lorenzo.antenucci@edu.unife.it>
2022-01-08 18:58:15 +0100

    Modifica grafica serra/create

--- resources/views/serra/create.blade.php
+++ resources/views/serra/create.blade.php
@@ -1,56 +1,55 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Creazione serra') }}</div>
 
-                <div class="card-body">
+<form action="{{ URL::action('SerraController@store') }}" method="POST" >
 
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+    <div class="jumbotron jumbotron-fluid" style="background-color: #1e90ff; margin-bottom: 0rem;">
 
-                    <form action="{{ URL::action('SerraController@store') }}" method="POST" >
-                        {{ csrf_field() }}
-                        <h1> Inserisci i dati della serra </h1>
-
-                        <input type="text" name="nome" placeholder="disneyland" value="{{ old('codice_serra') }}"/> <label> Nome </label>
-                        <br>
-                        
-
-                        <input type="hidden" id="lat" name="latitudine" placeholder="xx.xxxxxx" value="{{ old('latitudine') }}" />
-    
-
-                        <input type="hidden" id="lng" name="longitudine" placeholder="yy.yyyyyy" value="{{ old('longitudine') }}"/>
-                        
-
-                        <input type="hidden" name="capienza" value="20">
-
-                        @csrf
-                        <div>
-
-
-                            <div id="map" style="height:300px; width: 600px;" class="my-3"></div>
-
-                            <script src="{{ asset('js/posizione.js') }}"></script>
-                            <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBG8E5nHu7nZYmu5B0ONoerF4U5TZ2y2ao&callback=initMap"
-                                    type="text/javascript"></script>
-                        </div>
-
-                        <input type="submit" class="btn btn-primary" value="Crea serra"/>
-                    </form>
+        <div class="container">
+            @if ($errors->any())
+                <div class="alert alert-danger">
+                    <ul>
+                    @foreach ($errors->all() as $error)
+                        <li>{{ $error }}</li>
+                    @endforeach
+                    </ul>
                 </div>
-            </div>
+            @endif
+
+
+            {{ csrf_field() }}
+            <h1> Inserisci i dati della serra </h1>
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <span class="input-group-text" id="inputGroup-sizing-default">Nome Serra</span>
+                </div>
+                <input type="text" name="nome" value="{{ old('codice_serra') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+              </div>
+
+            <br>
+
+
+            <input type="hidden" id="lat" name="latitudine" placeholder="xx.xxxxxx" value="{{ old('latitudine') }}" />
+
+
+            <input type="hidden" id="lng" name="longitudine" placeholder="yy.yyyyyy" value="{{ old('longitudine') }}"/>
+
+
+            <input type="hidden" name="capienza" value="20">
+
+            @csrf
         </div>
     </div>
-</div>
+
+
+    <div id="map" style="height:450px; width: fill;" class="my-3"></div>
+
+        <script src="{{ asset('js/posizione.js') }}"></script>
+        <script async defer src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBG8E5nHu7nZYmu5B0ONoerF4U5TZ2y2ao&callback=initMap" type="text/javascript"></script>
+    </div>
+
+    <input type="submit" class="btn btn-primary" value="Crea serra"/>
+</form>
+
 @endsection
