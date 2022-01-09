81a3cccc95bb82cdf392e0f97ade7cb252d3bfcb
Lorenzo Antrenucci <lorenzo.antenucci@edu.unife.it>
2022-01-09 19:11:28 +0100

    Creazione vasi in serra index

--- resources/views/piantapostBackup02.blade.php
+++ resources/views/piantapostBackup02.blade.php
@@ -1,22 +1,13 @@
 <div class="col mb-4">
+    <div class="card">
 
-    <div style="width: 200px; display: block;
-    margin-left: auto;
-    margin-right: auto;">
         <a href="{{ URL::action('PiantaController@show', $pianta->codice_pianta) }}" >
         <?php
 
-        echo '<img src="data:image/jpeg;base64,'.base64_encode($pianta->foto).'" class="card-img-top" style=" width: 120px; height: 120px; display: block;
-        margin-left: auto;
-        margin-right: auto;"/>';
+        echo '<img src="data:image/jpeg;base64,'.base64_encode($pianta->foto).'" class="card-img-top" style="height: 250px;"/>';
 
         ?>
-        <img class="card-img-top" src="/immagini/vaso.png" border="0" class="icone">
         </a>
-
-    </div>
-    <br>
-    <div class="card">
       <div class="card-body">
         <h5 class="card-title">{{$pianta->nome}}</h5>
         <p>Luogo: {{$pianta->luogo}}</p>
@@ -27,7 +18,7 @@
                     @foreach($eventi as $d)
                         @if($d['nome'] == $b['nome'])
                             @if($dataoggi - strtotime($d['data']) > $b['cadenza'])
-                                <p style="color:rgb(145, 0, 0)">{{ $b['nome'] }}</p>
+                                {{ $b['nome'] }}
                             @endif
                         @endif
                     @endforeach
@@ -37,22 +28,24 @@
         </p>
         <p class="card-text">
 
+
+
+
+
+
         </p>
 
 
-                <form  action = "{{ URL::action('PiantaController@destroy', $pianta->codice_pianta)}}" method = "POST">
+                <form action = "{{ URL::action('PiantaController@destroy', $pianta->codice_pianta)}}" method = "POST">
 
                     {{ csrf_field() }}
                     {{ method_field('DELETE') }}
 
-                        <button type = "submit" style="background: none; border: none;"><img src="/immagini/delete.png" border="0" class="icone"></button>
-                        <a href = "{{ URL::action('PiantaController@edit', $pianta->codice_pianta)}}"><img src="/immagini/modifica.png" border="0" class="icone"></a>
-
-
+                    <button type = "submit" style="background: none; border: none;"><img src="/immagini/delete.png" border="0" class="icone"></button>
+                    <a href = "{{ URL::action('PiantaController@edit', $pianta->codice_pianta)}}"><img src="/immagini/modifica.png" border="0" class="icone"></a>
                     <br>
                     <br>
                 </form>
-
             <a href="{{ URL::action('DiarioController@index', $pianta->codice_pianta)}}"><img src="/immagini/diario.png" border="0" class="icone"></a>
 
             <a href="{{ URL::action('BisognoController@index', $pianta->codice_pianta) }}"> <img src="/immagini/bisogni.png" border="0" class="icone"> </a>
