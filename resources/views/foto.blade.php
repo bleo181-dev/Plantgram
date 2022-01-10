215e442d48ce9b93cd9ec1bbbd243ce9c5f55aed
Lorenzo Antrenucci <lorenzo.antenucci@edu.unife.it>
2022-01-10 18:31:46 +0100

    grafica pianta/create

--- resources/views/foto.blade.php
+++ resources/views/foto.blade.php
@@ -1,5 +1,8 @@
-<div class="card col-md-7">
-    <p>Carica una foto</p>
-    <input type="file" name="foto" id="imgInp"> <br>
+    <!-- <p>Carica una foto</p>
+    <input type="file" name="foto" id="imgInp"> <br> -->
+
+    <div class="custom-file">
+        <input type="file" name="foto" class="custom-file-input" id="imgInp" aria-describedby="inputGroupFileAddon01">
+        <label class="custom-file-label" for="inputGroupFile01">Carica una foto</label>
+    </div>
     <img class="imagePreviewPianta" id="blah" src="#"/>
-</div>
--- resources/views/pianta/create.blade.php
+++ resources/views/pianta/create.blade.php
@@ -6,58 +6,60 @@
     <br>
     <div id="cont" class="container">
 
-                    @if ($errors->any())
-                        <div class="alert alert-danger">
-                            <ul>
-                                @foreach ($errors->all() as $error)
-                                    <li>{{ $error }}</li>
-                                @endforeach
-                            </ul>
-                        </div>
-                    @endif
+        @if ($errors->any())
+            <div class="alert alert-danger">
+                <ul>
+                @foreach ($errors->all() as $error)
+                    <li>{{ $error }}</li>
+                @endforeach
+                </ul>
+            </div>
+        @endif
 
-                    <form action="{{ URL::action('PiantaController@store') }}" method="POST" enctype="multipart/form-data" >
-                        {{ csrf_field() }}
-                        <h1> Inserisci i dati della pianta </h1>
+        <form action="{{ URL::action('PiantaController@store') }}" method="POST" enctype="multipart/form-data" >
+            {{ csrf_field() }}
+            <h1> Inserisci i dati della pianta </h1>
+            <div class="input-group input-group-sm mb-3">
+                <div class="input-group mb-3">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Nome pianta</span>
+                    </div>
+                    <input id="namePlant" type="text" name="nome" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default" value="{{ old('nome') }}">
+                    <div class="input-group-prepend">
+                        <span class="input-group-text" id="inputGroup-sizing-default">Dove si trova?</span>
+                    </div>
+                    <input id="namePlant" type="text" name="luogo" placeholder="Giardino" value="{{ old('luogo') }}" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-default">
+                </div>
+            </div>
+            <br>
+            <br>
+            @include('foto') <!-- Serve per includere il capo foto e la sua preview -->
+            <br>
+            <br>
+            <h10> Usa AI per riconoscere la pianta </h1> <br>
 
-                        <!--<input type="number" name="codice_serra" placeholder="1111" value="{{ old('codice_serra') }}"/> <label> Codice serra </label>
-                        <br>
-                        <br>-->
-
-                        <input id="namePlant" type="text" name="nome" placeholder="Basilico" value="{{ old('nome') }}" /> <label> Nome pianta </label>
-                        <br>
-                        <br>
-
-                        <input type="text" name="luogo" placeholder="Giardino" value="{{ old('luogo') }}"/> <label> Dove si trova </label>
-                        <br>
-                        <br>
-                        @include('foto') <!-- Serve per includere il capo foto e la sua preview -->
-                        <br>
-                        <br>
-                        <h10> Usa AI per riconoscere la pianta </h1> <br>
-
-                        <img id = "similarIm" src = "" class="imagePreviewPianta">
-                        <br>
-
-
-                        <button id="cerca" type="button"><img src="{{ asset('immagini/occhio.png') }}" class="card-img-top" /></button><br>
-
-                        Visibilità
-                        <br>
-                        <input type="radio" name="stato" value="0" /> privata
-                        <br>
-                        <input type="radio" name="stato" value="1" /> pubblica
-                        <br>
-                        <br>
-
-                        <input type="submit" value="Aggiungi pianta" />
-
-                        <br>
-
-                        <h10 id="description">  </h1> <br>
+            <img id = "similarIm" src = "" class="imagePreviewPianta">
+            <br>
 
 
-                    </form>
+            <button id="cerca" type="button"><img src="{{ asset('immagini/occhio.png') }}" class="card-img-top" /></button><br>
+
+            Visibilità
+            <br>
+            <input type="radio" name="stato" value="0" /> privata
+            <br>
+            <input type="radio" name="stato" value="1" /> pubblica
+            <br>
+            <br>
+
+            <input type="submit" value="Aggiungi pianta" />
+
+            <br>
+
+            <h10 id="description">  </h1> <br>
+
+
+        </form>
     </div>
 </div>
 
