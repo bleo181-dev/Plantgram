9b1afdfc44a1e7304acab805b595dd3ad5bb88a1
riccardo <riccardo01.luppi@edu.unife.it>
2022-01-11 09:56:48 +0100

    n

--- resources/views/diario/index.blade.php
+++ resources/views/diario/index.blade.php
@@ -3,17 +3,19 @@
 @section('content')
 <div class="container">
     <div class="row justify-content-center">
-        <div class="card">
-            <div class="card-header"> Diario di {{ $pianta->nome }}</div>
+        <div class ="col-md-8">
+            <div class="card">
+                <div class="card-header"> Diario di {{ $pianta->nome }}</div>
 
-                <div class="card-body">
-                    <div style="padding: 10px;"> 
-                        <a href="{{ URL::action('DiarioController@create', $id) }}" class="btn btn-success"> Crea ricordo</a>
-                    </div>
-                    <div class="row">
-                        @foreach($diario as $i)
-                            @include('diario.diariopost')
-                        @endforeach
+                    <div class="card-body">
+                        <div style="padding: 10px;"> 
+                            <a href="{{ URL::action('DiarioController@create', $id) }}" class="btn btn-success"> Crea ricordo</a>
+                        </div>
+                        <div class="row">
+                            @foreach($diario as $i)
+                                @include('diario.diariopost')
+                            @endforeach
+                        </div>
                     </div>
                 </div>
             </div>
