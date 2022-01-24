7e6bac779fc2eaab1b16a24d6b760c8c8f098fc1
riccardo <riccardo01.luppi@edu.unife.it>
2022-01-24 22:45:31 +0100

    prova grafici

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,9 @@
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
 
+    <!-- Ploty -->
+    <script src="https://cdn.plot.ly/plotly-latest.min.js"></script>
+
 </head>
 <body>
     <div id="app">
--- resources/views/pianta/show.blade.php
+++ resources/views/pianta/show.blade.php
@@ -8,15 +8,15 @@
                 <div class="card-header">info</div>
                 <div class="card-body">
                     
-                        @if(auth()->id() == $serra->codice_utente)
-                        <div style="padding:10px">
-                            <a href="{{ URL::action('SerraController@index') }}" class="btn btn-secondary">Indietro</a>
-                        </div>
-                        @else
-                        <div style="padding:10px">
-                            <a href="{{URL::action('SerraController@indexserrashare', $pianta->codice_serra)  }}" class="btn btn-secondary">Indietro</a>
-                        </div>
-                        @endif
+                    @if(auth()->id() == $serra->codice_utente)
+                    <div style="padding:10px">
+                        <a href="{{ URL::action('SerraController@index') }}" class="btn btn-secondary">Indietro</a>
+                    </div>
+                    @else
+                    <div style="padding:10px">
+                        <a href="{{URL::action('SerraController@indexserrashare', $pianta->codice_serra)  }}" class="btn btn-secondary">Indietro</a>
+                    </div>
+                    @endif
                     <div class="row no-gutters">
                         <div class="col-md-4">
                             <?php
@@ -36,7 +36,7 @@
                     <br>
                 
                     @foreach($eventi as $evento)
-                        <div class="row justify-content-center">
+                    <div class="row justify-content-center">
                             
                             <!-- vecchia progressbar
                             <div class="progress w-75">
@@ -50,37 +50,57 @@
 
                             <a data-toggle="modal" data-target="#staticBackdrop{{$evento->codice_evento}}"class=" col-md-2 btn btn-primary ">{{$evento->nome}}</a>
 
-                                <!-- Modal -->
-                                <div class="modal fade" id="staticBackdrop{{$evento->codice_evento}}" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
-                                    <div class="modal-dialog">
-                                        <div class="modal-content">
-                                            <div class="modal-header">
-                                                <h5 class="modal-title" id="staticBackdropLabel">{{$evento->nome}}</h5>
-                                                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-                                                <span aria-hidden="true">&times;</span>
-                                                </button>
-                                            </div>
-                                            <div class="modal-body">
-                                                Sei sicuro di aver dato cio di cui la pianta ha bisogno?
-                                            </div>
-                                            <div class="modal-footer">
-                                                <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
-                                                <form action="{{ URL::action('EventoController@store', $evento->codice_evento) }}" method="POST" class="col-md-2">
-                                                    {{ csrf_field() }}
-                                                    @method('PUT')
-                                                    <input type="hidden" name="nome" value="{{$evento->nome}}">
-                                                    <button class="btn btn-primary" type="submit">Si</button>
-                                                </form>
-                                            </div>
+                            <!-- Modal -->
+                            <div class="modal fade" id="staticBackdrop{{$evento->codice_evento}}" data-backdrop="static" data-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel" aria-hidden="true">
+                                <div class="modal-dialog">
+                                    <div class="modal-content">
+                                        <div class="modal-header">
+                                            <h5 class="modal-title" id="staticBackdropLabel">{{$evento->nome}}</h5>
+                                            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                                            <span aria-hidden="true">&times;</span>
+                                            </button>
+                                        </div>
+                                        <div class="modal-body">
+                                            Sei sicuro di aver dato cio di cui la pianta ha bisogno?
+                                        </div>
+                                        <div class="modal-footer">
+                                            <button type="button" class="btn btn-secondary" data-dismiss="modal">No</button>
+                                            <form action="{{ URL::action('EventoController@store', $evento->codice_evento) }}" method="POST" class="col-md-2">
+                                                {{ csrf_field() }}
+                                                @method('PUT')
+                                                <input type="hidden" name="nome" value="{{$evento->nome}}">
+                                                <button class="btn btn-primary" type="submit">Si</button>
+                                            </form>
                                         </div>
                                     </div>
                                 </div>
-                                <!--endmodal-->
+                            </div>
+                            <!--endmodal-->
+                            
+                            <br>
 
-                            </form>
-                        </div>
-                        <br>
-                       <!-- <canvas id="myChart" style="width:100%;max-width:700px; height: 300px;"></canvas>-->
+                            <!-- explode -->
+                            <div class="w-75 accordion" id="accordionExample{{$evento->nome}}">
+                            <br>
+                                <div class="card">
+                                    <div class="card-header" id="headingOne">
+                                        <h2 class="mb-0">
+                                            <button class="btn btn-link btn-block text-left" type="button" data-toggle="collapse" data-target="#{{$evento->nome}}" aria-expanded="true" aria-controls="collapseOne{{$evento->nome}}">
+                                            visualizza grafico
+                                            </button>
+                                        </h2>
+                                    </div>
+
+                                    <div id="{{$evento->nome}}" class="collapse show" aria-labelledby="headingOne" data-parent="#accordionExample{{$evento->nome}}">
+                                        <div class="card-body">
+                                            <!-- plot -->
+                                            <div id="myPlot" style="width:100%;max-width:700px"></div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <!--endexplode-->                                  
+                        </div>  
                         <br>
                     @endforeach
                     @foreach($diario as $i)
@@ -92,20 +112,21 @@
     </div>
 </div>
 <script>
-var xyValues = [
-  {x:50, y:7},
-  {x:60, y:8},
-  {x:70, y:8},
-  {x:80, y:9},
-  {x:90, y:9},
-  {x:100, y:9},
-  {x:110, y:10},
-  {x:120, y:11},
-  {x:130, y:14},
-  {x:140, y:14},
-  {x:150, y:15}
-];
+    var xArray = ["Italy", "France", "Spain", "USA", "Argentina"];
+    var yArray = [55, 49, 44, 24, 15];
 
+    var data = [{
+    x:xArray,
+    y:yArray,
+    type:"bar"
+    }];
+
+    var layout = {title:"World Wide Wine Production"};
+
+    Plotly.newPlot("myPlot", data, layout);
+</script>
+
+<script>
 new Chart("myChart", {
   type: "scatter",
   data: {
