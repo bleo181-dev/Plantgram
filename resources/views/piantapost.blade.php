9d8566e70bc066a550198f8645b485b05a338511
Lorenzo <lorenzo.antenucci@edu.unife.it>
2022-02-18 14:47:11 +0100

    Flexino preview immagine in serra index: ora l'immagine non viene stretchata viene solo ritagliata, così da preservare la qualità, ed essere bello esteticamente

--- resources/views/piantapost.blade.php
+++ resources/views/piantapost.blade.php
@@ -1,16 +1,18 @@
 <div class="col mb-4">
     <div class="card">
         <a href="{{ URL::action('PiantaController@show', $pianta->codice_pianta) }}" >
-            <?php
-                echo '<img src="data:image/jpeg;base64,'.base64_encode($pianta->foto).'" class="card-img-top" style="height: 350px;"/>';
-            ?>
-        </a>
-    <div class="card-body">
-        <div class="row">
-            <div class="col-10 text-truncate">
-                <h5 class="card-title">{{$pianta->nome}}</h5>
+            <div class="plant">
+                <?php
+                    echo '<img src="data:image/jpeg;base64,'.base64_encode($pianta->foto).'" class="card-img-top" />';
+                ?>
             </div>
-        </div>
+        </a>
+        <div class="card-body">
+            <div class="row">
+                <div class="col-10 text-truncate">
+                    <h5 class="card-title">{{$pianta->nome}}</h5>
+                </div>
+            </div>
             <!-- bisogni arrettrati -->
             @foreach($bisogni as $b)
                 @foreach($eventi as $d)
@@ -26,7 +28,7 @@
                 @endforeach
             @endforeach
             <!-- fine bisogni arrettrati -->
-      </div>
+        </div>
     </div>
 </div>
 
