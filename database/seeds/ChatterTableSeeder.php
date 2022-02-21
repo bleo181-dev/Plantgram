14f98016fd9b03628ee7e28145204a807622498f
Leonardo Bombonati <leonardo.bombonati@edu.unife.it>
2022-02-21 10:47:18 +0100

    fix seed 3

--- database/seeds/ChatterTableSeeder.php
+++ database/seeds/ChatterTableSeeder.php
@@ -11,9 +11,6 @@ class ChatterTableSeeder extends Seeder
      */
     public function run()
     {
-
-
-
         // CREATE THE CATEGORIES
 
         \DB::table('chatter_categories')->delete();
@@ -60,49 +57,5 @@ class ChatterTableSeeder extends Seeder
                 'updated_at' => null,
             ],
         ]);
-
-        // CREATE THE DISCUSSIONS
-
-        \DB::table('chatter_discussion')->delete();
-
-        \DB::table('chatter_discussion')->insert([
-            0 => [
-                'id'                  => 3,
-                'chatter_category_id' => 1,
-                'title'               => 'Titolo post',
-                'user_id'             => 1,
-                'sticky'              => 0,
-                'views'               => 0,
-                'answered'            => 0,
-                'created_at'          => '2016-08-18 14:27:56',
-                'updated_at'          => '2016-08-18 14:27:56',
-                'slug'                => 'titolo post',
-                'color'               => '#239900',
-            ]
-        ]);
-
-        // CREATE THE POSTS
-
-        \DB::table('chatter_post')->delete();
-
-        \DB::table('chatter_post')->insert([
-                    0 => [
-                        'id'                    => 1,
-                        'chatter_discussion_id' => 3,
-                        'user_id'               => 1,
-                        'body'                  => '<p>Ciao, questo è un post! foto piedini nei commenti???</p>',
-                        'created_at' => '2022-01-23 14:27:56',
-                        'updated_at' => '2022-01-24 14:27:56',
-                    ],
-                    1 => [
-                        'id'                    => 2,
-                        'chatter_discussion_id' => 3,
-                        'user_id'               => 1,
-                        'body'                  => '<p>Beccati sta GIF di Keanu Reeves</p>
-                    <p><img src="https://media.giphy.com/media/j5QcmXoFWl4Q0/giphy.gif" alt="" width="366" height="229" /></p>',
-                        'created_at' => '2022-01-25 15:01:25',
-                        'updated_at' => '2022-01-25 15:01:25',
-                    ]
-                ]);
     }
 }
